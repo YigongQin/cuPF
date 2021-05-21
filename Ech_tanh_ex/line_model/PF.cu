cd80c2b30eb5a656bf222d844ef9a228392893ff
Yigong Qin <ygqin@login2.longhorn.tacc.utexas.edu>
2021-05-21 10:04:20 -0500

    update functions

--- Ech_tanh_ex/line_model/PF.cu
+++ Ech_tanh_ex/line_model/PF.cu
@@ -6,6 +6,7 @@
 #include <curand.h>
 #include <curand_kernel.h>
 #include "CycleTimer.h"
+#include "include_struct.h"
 #define LS -0.995
 #define TIPP 100
 #define BLANK 0.3
@@ -21,61 +22,6 @@ extern float toBW(int bytes, float sec);
 
 //__managed__ float* meanx;
 
-struct GlobalConstants {
-  int nx;
-  int ny;
-  int Mt;
-  int nts; 
-  int ictype;
-  float G;
-  float R;
-  float delta;
-  float k;
-  float c_infm;
-  float Dl;
-  float d0;
-  float W0;
-  float lT;
-  float lamd; 
-  float tau0;
-  float c_infty; 
-  float R_tilde;
-  float Dl_tilde; 
-  float lT_tilde; 
-  float eps; 
-  float alpha0; 
-  float dx; 
-  float dt; 
-  float asp_ratio; 
-  float lxd;
-  float lx; 
-  float lyd; 
-  float eta; 
-  float U0; 
-  int seed_val;
-  // parameters that are not in the input file
-  float hi;
-  float cosa;
-  float sina;
-  float sqrt2;
-  float a_s;
-  float epsilon;
-  float a_12;
-  float dt_sqrt;
-  int noi_period;
-};
-
-struct Mac_input{
-  int Ny;
-  int Nt;
-  float* t_mac;
-  float* y_mac;
-  float* Gt;
-  float* Rt;
-  float* psi_mac;
-  float* U_mac;
-
-};
 
 __constant__ GlobalConstants cP;
 
--- Ech_tanh_ex/line_model/main.cpp
+++ Ech_tanh_ex/line_model/main.cpp
@@ -18,66 +18,7 @@
 #include <hdf5.h>
 // #include <mat.h> 
 using namespace std;
-
-
-void printCudaInfo();
-
-struct GlobalConstants {
-  int nx;
-  int ny;
-  int Mt;
-  int nts; 
-  int ictype;
-  float G;
-  float R;
-  float delta;
-  float k;
-  float c_infm;
-  float Dl;
-  float d0;
-  float W0;
-  float lT;
-  float lamd; 
-  float tau0;
-  float c_infty; 
-  float R_tilde;
-  float Dl_tilde; 
-  float lT_tilde; 
-  float eps; 
-  float alpha0; 
-  float dx; 
-  float dt; 
-  float asp_ratio; 
-  float lxd;
-  float lx; 
-  float lyd; 
-  float eta; 
-  float U0; 
-  int seed_val;
-  // parameters that are not in the input file
-  float hi;
-  float cosa;
-  float sina;
-  float sqrt2;
-  float a_s;
-  float epsilon;
-  float a_12;
-  float dt_sqrt;
-  int noi_period;
-};
-
-
-struct Mac_input{
-  int Ny;
-  int Nt;
-  float* t_mac;
-  float* y_mac;
-  float* Gt; 
-  float* Rt; 
-  float* psi_mac;
-  float* U_mac;
-
-};
+#include "include_struct.h"
 
 
 void setup(Mac_input mac, GlobalConstants params, int fnx, int fny, float* x, float* y, float* phi, float* psi,float* U);
