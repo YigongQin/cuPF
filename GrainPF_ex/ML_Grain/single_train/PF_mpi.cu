fb96205498f2952c35862e05217a524e2d1fa62a
Yigong Qin <yigongqin@wireless-10-145-107-111.public.utexas.edu>
2021-12-13 13:27:48 -0600

    clear qoi

--- GrainPF_ex/ML_Grain/single_train/PF_mpi.cu
+++ GrainPF_ex/ML_Grain/single_train/PF_mpi.cu
@@ -821,20 +821,9 @@ void calc_qois(int* cur_tip, int* alpha, int fnx, int fny, int kt, int num_grain
             int C = fnx*j + i;
               if (alpha[C]>0){ 
                 tip_final[kt*num_grains+alpha[C]-1] = j; 
-                total_area[kt*num_grains+alpha[C]-1]+=1;}
+                total_area[kt*num_grains+alpha[C]-1]+=1;
+                if (j > *cur_tip) {extra_area[kt*num_grains+alpha[C]-1]+=1; }}
          }
-     }
-
-     for (int j = *cur_tip+1; j<fny-1; j++){ 
-
-         for (int i=1; i<fnx-1;i++){
-
-            int C = fnx*j + i;
-
-              if (alpha[C]>0){ extra_area[kt*num_grains+alpha[C]-1]+=1; }
-
-         }
-
      }
 
 
