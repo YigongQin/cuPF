579f859d08c1abfbbbda977215e9c747166dc1c3
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2021-12-18 15:11:26 -0600

    3D

--- GrainPF_ex/ML_Grain/3D/main.cpp
+++ GrainPF_ex/ML_Grain/3D/main.cpp
@@ -589,9 +589,9 @@ int main(int argc, char** argv)
     memset(total_area, 0, sizeof(int)*(params.nts+1)*params.num_theta ); 
     memset(tip_final,  0, sizeof(int)*(params.nts+1)*params.num_theta ); 
 
-    setup( pM, params, mac, length_x, length_y, length_z, length_z_full, x, y, z, phi, psi, Uc, alpha_i, alpha_i_full, tip_y, frac, aseq, extra_area, tip_final, total_area);
-    for(int i=0; i<length_y; i++){
-        y[i]=(i-params.ha_wd)*dxd + ymin_loc;
+    //setup( pM, params, mac, length_x, length_y, length_z, length_z_full, x, y, z, phi, psi, Uc, alpha_i, alpha_i_full, tip_y, frac, aseq, extra_area, tip_final, total_area);
+    for(int i=0; i<length_z; i++){
+        z[i]=(i-params.ha_wd)*dxd + zmin_loc;
     }
     // save the QoIs 
     //float* tip_y_asse=(float*) malloc(num_case*(params.nts+1)* sizeof(float));
@@ -644,7 +644,7 @@ int main(int argc, char** argv)
 
     h5_file = H5Fcreate(out_file.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
-
+    h5write_1d(h5_file, "phi",      phi , length, "float");
     h5write_1d(h5_file, "alpha",    alpha_asse, valid_run*full_length, "int");
     h5write_1d(h5_file, "sequence", aseq_asse, num_case*params.num_theta, "int");
 
