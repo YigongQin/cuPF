c0bddb7ac7729523cf030198191e7e60e2eb43df
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2021-12-18 15:33:58 -0600

    3D

--- GrainPF_ex/ML_Grain/3D/main.cpp
+++ GrainPF_ex/ML_Grain/3D/main.cpp
@@ -591,7 +591,7 @@ int main(int argc, char** argv)
     memset(total_area, 0, sizeof(int)*(params.nts+1)*params.num_theta ); 
     memset(tip_final,  0, sizeof(int)*(params.nts+1)*params.num_theta ); 
 
-    //setup( pM, params, mac, length_x, length_y, length_z, length_z_full, x, y, z, phi, psi, Uc, alpha_i, alpha_i_full, tip_y, frac, aseq, extra_area, tip_final, total_area);
+    setup( pM, params, mac, length_x, length_y, length_z, length_z_full, x, y, z, phi, psi, Uc, alpha_i, alpha_i_full, tip_y, frac, aseq, extra_area, tip_final, total_area);
     for(int i=0; i<length_z; i++){
         z[i]=(i-params.ha_wd)*dxd + zmin_loc;
     }
@@ -611,7 +611,7 @@ int main(int argc, char** argv)
 
     if (run>=num_case-valid_run){
         int loca_case = run-(num_case-valid_run);
-           memcpy(alpha_asse+loca_case*full_length,alpha_i_full,sizeof(int)*full_length);
+           memcpy(alpha_asse+loca_case*full_length,alpha_i,sizeof(int)*full_length);
 
     }   
 
