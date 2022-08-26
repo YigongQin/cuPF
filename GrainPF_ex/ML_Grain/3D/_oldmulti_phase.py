c49781092baa5ab765b9818b6a1e730fb5ac3198
Yigong Qin <ygqin@login2.longhorn.tacc.utexas.edu>
2022-08-25 22:01:15 -0500

    input file

---
+++ GrainPF_ex/ML_Grain/3D/_oldmulti_phase.py
@@ -0,0 +1,57 @@
+import numpy as np
+import scipy.io as sio
+import h5py
+import sys
+
+nx = 11
+ny = 11
+nz = 11
+nt = 11
+Lx = 10
+Ly = 10
+Lz = 10
+BC = 1
+top = 7
+
+
+G = float(sys.argv[1]);
+Rmax = 1e6*float(sys.argv[2]);
+z0 = 2
+
+x = np.linspace(0-BC,Lx+BC,nx)
+y = np.linspace(0-BC,Ly+BC,ny)
+z = np.linspace(0-BC,Lz+BC,nz)
+t = np.linspace(0,top/Rmax,nt)
+tmax = t[-1]
+
+T = np.zeros(nx*ny*nz*nt)
+alpha = np.zeros(nx*ny*nz)
+psi = np.zeros(nx*ny*nz)
+U = np.zeros(nx*ny*nz)
+
+
+for i in range(nx*ny*nz*nt):
+    
+    xi = i%nx
+    yi = int( (i%(nx*ny))/nx )
+    zi = int( (i%(nx*ny*nz))/(nx*ny) )
+    ti = int(i/(nx*ny*nz))
+    
+    #T[i] = 920 + G*( y[yi] - 0.5*Rmax*(t[ti]**2/tmax) - y0)
+    T[i] = 920 + G*( z[zi] - Rmax*t[ti] - z0)    
+    if i==nx*ny*nz*nt-1: print(T[i], G, z[zi], Rmax, t[ti], z0)
+    if ti==0:
+       psi[i] = z0 - z[zi]      
+        
+mac_folder = 'line_AM/'    
+np.savetxt(mac_folder+'x.txt', x, fmt='%1.4e',delimiter='\n') 
+np.savetxt(mac_folder+'y.txt', y, fmt='%1.4e',delimiter='\n')
+np.savetxt(mac_folder+'z.txt', y, fmt='%1.4e',delimiter='\n')
+np.savetxt(mac_folder+'t.txt', t, fmt='%1.4e',delimiter='\n')
+np.savetxt(mac_folder+'psi.txt', psi, fmt='%1.4e',delimiter='\n')
+np.savetxt(mac_folder+'U.txt', U, fmt='%1.4e',delimiter='\n')
+np.savetxt(mac_folder+'G.txt', np.asarray([G]), fmt='%1.4e',delimiter='\n')
+np.savetxt(mac_folder+'Rmax.txt', np.asarray([Rmax*1e-6]), fmt='%1.4e',delimiter='\n')
+hf = h5py.File(mac_folder+'Temp.h5', 'w')
+hf.create_dataset('Temp', data=T)
+hf.close()
