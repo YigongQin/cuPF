4d3dbddcdb46fb9aed3ba5ab983e0c02ec81b602
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2022-08-30 11:05:16 -0500

    clean

---
+++ GrainPF_ex/ML_Grain/3D/helper.cpp
@@ -0,0 +1,40 @@
+void print2d(float* array, int fnx, int fny);
+void printCudaInfo(int rank, int i);
+
+void print2d(float* array, int fnx, int fny){
+
+   int length = fnx*fny;
+   float* cpu_array = new float[fnx*fny];
+
+   cudaMemcpy(cpu_array, array, length * sizeof(float),cudaMemcpyDeviceToHost);
+
+   for (int i=0; i<length; i++){
+       if (i%fnx==0) printf("\n");
+       printf("%4.2f ",cpu_array[i]);
+   }
+
+}
+
+
+
+void printCudaInfo(int rank, int i)
+{
+    // for fun, just print out some stats on the machine
+
+    int deviceCount = 0;
+    cudaError_t err = cudaGetDeviceCount(&deviceCount);
+
+    printf("---------------------------------------------------------\n");
+    printf("Found %d CUDA devices\n", deviceCount);
+
+
+        cudaDeviceProp deviceProps;
+        cudaGetDeviceProperties(&deviceProps, i);
+        printf("rank %d, Device %d: %s\n", rank, i, deviceProps.name);
+        printf("   SMs:        %d\n", deviceProps.multiProcessorCount);
+        printf("   Global mem: %.0f MB\n",
+               static_cast<float>(deviceProps.totalGlobalMem) / (1024 * 1024));
+        printf("   CUDA Cap:   %d.%d\n", deviceProps.major, deviceProps.minor);
+
+    printf("---------------------------------------------------------\n"); 
+}
