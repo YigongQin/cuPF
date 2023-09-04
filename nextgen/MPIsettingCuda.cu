93a71ac24cf99bc9ecf9a2e15e74dc60175ec639
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2023-09-04 09:23:35 -0500

    mpi

--- nextgen/MPIsettingCuda.cu
+++ nextgen/MPIsettingCuda.cu
@@ -2,6 +2,57 @@
 #include "devicefunc.cu_inl"
 #include <algorithm>
 
+__global__ void
+collectData1D(MPIsetting* p, float* field, int numFields, int offset)
+{
+    int C = blockIdx.x * blockDim.x + threadIdx.x;
+    int i, j, k, PF_id, fnx, fny, fnz;
+    int nxLocal = p->nxLocal;
+    int nyLocal = p->nyLocal;
+    int nzLocal = p->nzLocal;
+    int haloWidth = p->haloWidth;
+    G2L_3D(C, i, j, k, PF_id, nxLocal, nyLocal, nzLocal);
+    if ( (i<haloWidth) && (j<nyLocal) && (k<nzLocal) && (PF_id<numFields))
+    {
+        fnx = nxLocal + haloWidth*2;
+        fny = nyLocal + haloWidth*2;
+        fnz = nzLocal + haloWidth*2;
+
+        int field_indexL = L2G_4D(i + haloWidth, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
+        int field_indexR = L2G_4D(i + nxLocal, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
+
+        p->mMPIBuffer["sendL"].first[C + offset] = field[field_indexL];
+        p->mMPIBuffer["sendR"].first[C + offset] = field[field_indexR];
+    }
+}
+
+__global__ void
+distributeData1D(MPIsetting* p, float* field, int numFields, int offset)
+{
+  int C = blockIdx.x * blockDim.x + threadIdx.x;
+  int i, j, k, PF_id, fnx, fny, fnz;
+  int nxLocal = p->nxLocal;
+  int nyLocal = p->nyLocal;
+  int nzLocal = p->nzLocal;
+  int haloWidth = p->haloWidth;
+  G2L_3D(C, i, j, k, PF_id, nxLocal, nyLocal, nzLocal);
+
+  if ( (i<haloWidth) && (j<nyLocal) && (k<nzLocal) && (PF_id<numFields))
+  {
+      fnx = nxLocal + haloWidth*2;
+      fny = nyLocal + haloWidth*2;
+      fnz = nzLocal + haloWidth*2;
+
+      int field_indexL = L2G_4D(i, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
+      int field_indexR = L2G_4D(i + nxLocal + haloWidth, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
+
+      field[field_indexL] = p->mMPIBuffer["recvL"].first[C + offset];
+      field[field_indexR] = p->mMPIBuffer["recvR"].first[C + offset];
+  }
+}
+
+
+
 void MPIsetting::MPItransferData(int nTimeStep, std::vector<std::pair<float*, int>> fieldChunks)
 {
     int blocksize_2d = 128;  // seems reduce the block size makes it a little faster, but around 128 is okay.
@@ -10,8 +61,7 @@ void MPIsetting::MPItransferData(int nTimeStep, std::vector<std::pair<float*, in
         int threadsRequired = field.second*std::max_element(mGeometrySize.begin(), mGeometrySize.end());
         int dataAcquired = 0;
         int num_block_2d = (threadsRequired + blocksize_2d -1)/blocksize_2d;
-        collectData1D<<< num_block_2d, blocksize_2d >>>(field.first, field.second, dataAcquired, 
-                                                        nxLocal, nyLocal, nzLocal, haloWidth);
+        collectData1D<<< num_block_2d, blocksize_2d >>>(this, field.first, field.second);
         dataAcquired += field.second*mGeometrySize[0];
     }
 
@@ -24,58 +74,9 @@ void MPIsetting::MPItransferData(int nTimeStep, std::vector<std::pair<float*, in
         int threadsRequired = field.second*std::max_element(mGeometrySize.begin(), mGeometrySize.end());
         int dataAcquired = 0;
         int num_block_2d = (threadsRequired + blocksize_2d -1)/blocksize_2d;
-        distributeData1D<<< num_block_2d, blocksize_2d >>>(field.first, field.second, dataAcquired,
-                                                           nxLocal, nyLocal, nzLocal, haloWidth);
+        distributeData1D<<< num_block_2d, blocksize_2d >>>(this, field.first, field.second);
         dataAcquired += field.second*mGeometrySize[0];
     }
 
     cudaDeviceSynchronize();      
 }
-
-__global__ void
-collectData1D(float* field, int numFields, int offset, 
-              int nxLocal, int nyLocal, int nzLocal, int haloWidth)
-{
-    int C = blockIdx.x * blockDim.x + threadIdx.x;
-    int i, j, k, PF_id, fnx, fny, fnz;
-    G2L_3D(C, i, j, k, PF_id, nxLocal, nyLocal, nzLocal);
-    if ( (i<haloWidth) && (j<nyLocal) && (k<nzLocal) && (PF_id<numFields))
-    {
-        fnx = nxLocal + haloWidth*2;
-        fny = nyLocal + haloWidth*2;
-        fnz = nzLocal + haloWidth*2;
-        // int field_indexL = i+hd+(j+hd)*fnx;
-        int field_indexL = L2G_4D(i + haloWidth, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
-        int field_indexR = L2G_4D(i + nxLocal, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
-
-        mMPIBuffer["sendL"].first[C + offset] = field[field_indexL];
-        mMPIBuffer["sendR"].first[C + offset] = field[field_indexR];
-    }
-}
-
-__global__ void
-distributeData1D(float* field, int numFields, int offset, 
-                 int nxLocal, int nyLocal, int nzLocal, int haloWidth)
-{
-  int C = blockIdx.x * blockDim.x + threadIdx.x;
-  int i, j, k, PF_id, fnx, fny, fnz;
-  fnx = nxLocal + haloWidth*2;
-  fny = nyLocal + haloWidth*2;
-  fnz = nzLocal + haloWidth*2;
-
-  G2L_3D(C, i, j, k, PF_id, nxLocal, nyLocal, nzLocal);
-
-  if ( (i<haloWidth) && (j<nyLocal) && (k<nzLocal) && (PF_id<numFields))
-  {
-
-      // int field_indexL = i+hd+(j+hd)*fnx;
-      int field_indexL = L2G_4D(i, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
-      int field_indexR = L2G_4D(i + nxLocal + haloWidth, j + haloWidth, k + haloWidth, PF_id, fnx, fny, fnz);
-
-      field[field_indexL] = mMPIBuffer["recvL"].first[C + offset];
-      field[field_indexR] = mMPIBuffer["recvR"].first[C + offset];
-  }
-}
-
-
-
