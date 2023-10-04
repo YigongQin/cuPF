c45d659a40461570bbb0f37314755937b65e9e09
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2023-10-04 15:47:54 -0500

    add no flux boundary

--- 3DgrainMPI/grains3D.py
+++ 3DgrainMPI/grains3D.py
@@ -10,17 +10,17 @@ from TemperatureProfile3DAnalytic import ThermalProfile
 Tmelt = 933.3
 Dh = 8.43e7                     # heat diffusion
 L_cp = 229
-GT = 0.347                      # Gibbs-Thompson coefficient K*um
+GT = 0.347                      # Gibbs-Thompson coefficient [K*um]
 beta0 = 1e-7                    # linear coefficient
 mu_k = 0.217e6                  # [um/s/K]
 delta = 0.01                    # strength of the surface tension anisotropy         
-kin_delta = 0.11
+kin_delta = 0.11                # kinetic anisotropy
 
 
 # nuleation parameters
 undcool_mean = 2                # Kelvin  nuleantion barrier
 undcool_std = 0.5               # Kelvin fixed
-nuc_Nmax = 0.01                 # 1/um^2 density; 0 to very big number 
+nuc_Nmax = 0.01                 # density [1/um^2] 
 nuc_rad = 0.4                   # radius of a nucleai
 
 # macro grid parameters
@@ -39,17 +39,17 @@ zmin = 0
 ## noise
 eta = 0.0  
 noi_period = 200
-eps = 1e-8                       # divide-by-zero treatment
-ictype = 0                    # initial condtion
+eps = 1e-8                      # divide-by-zero treatment
+ictype = 0                      # initial condtion
 
 # simulation parameters
-dx = 0.8                            # mesh width
-W0 = 0.1                    # interface thickness      um
-cfl = 1.0
-asp_ratio_yx = 0.25
-asp_ratio_zx = 0.5                    # aspect ratio
+dx = 0.8                        # mesh width
+W0 = 0.1                        # interface thickness [um]
+cfl = 1.0                       # cfl number
+asp_ratio_yx = 0.25             # aspect ratio of domain z/x
+asp_ratio_zx = 0.5              # aspect ratio of domain z/x
 moving_ratio = 0.2
-nts = 1          # number snapshots to save, Mt/nts must be int
+nts = 1                         # number snapshots to save, Mt/nts must be int
 Lx = 40
 Ly = Lx*asp_ratio_yx
 Lz = Lx*asp_ratio_zx
@@ -124,8 +124,8 @@ if __name__ == '__main__':
     
     
     '''create a planar graph'''
-    
-    g1 = graph(lxd = Lx, seed = seed) 
+    BC = 'periodic' if (args.boundary)[:2] == '11' else 'noflux'
+    g1 = graph(lxd = Lx, seed = seed, BC = BC) 
     print('input shape of alpha_field, ', g1.alpha_field.shape)
     
     alpha = g1.alpha_field
