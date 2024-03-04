e89c8c582073d8640d10ff2d4e1ebea3d35edd32
Yigong Qin <yigongqin@Yigongs-MacBook-Pro.local>
2024-03-04 10:38:10 -0600

    cat data

--- 3DgrainMPI/saveVTKdata.py
+++ 3DgrainMPI/saveVTKdata.py
@@ -16,21 +16,21 @@ from tvtk.api import tvtk, write_data
 
 class grain_visual:
     def __init__(self, 
-                 rank: int = 0,
-                 maxRank: int = 2,
+                 gpus: int = 1,
                  lxd: float = 40, 
                  seed: int = 1, 
                  frames: int = 1, 
                  height: int = -1,
                  time: int = 0,
+                 subsample: int = 2,
                  physical_params = {}):   
 
-        self.rank = rank
-        self.maxRank = maxRank
+        self.gpus = gpus
         self.lxd = lxd
         self.seed = seed
         self.height = height
         self.time = time
+        self.subsample = subsample
         self.base_width = 2
         self.frames = frames # note that frames include the initial condition
         self.physical_params = physical_params
@@ -38,58 +38,68 @@ class grain_visual:
 
     def load(self, rawdat_dir: str = './'):
        
+        x_list = []
+        alpha_list = []
+       
+        for rank in range(self.gpus):
+            
+            self.data_file = (glob.glob(rawdat_dir + '/*seed'+str(self.seed)+ '*rank'+str(rank) + '_' + '*time'+str(self.time) + '*.h5'))[0]
+            f = h5py.File(self.data_file, 'r')
+            
+            self.x = np.asarray(f['x_coordinates'])
+            self.y = np.asarray(f['y_coordinates'])
+            self.z = np.asarray(f['z_coordinates']) 
         
-        self.data_file = (glob.glob(rawdat_dir + '/*seed'+str(self.seed)+ '*rank'+str(self.rank) + '_' + '*time'+str(self.time) + '*.h5'))[0]
-        f = h5py.File(self.data_file, 'r')
-        self.x = np.asarray(f['x_coordinates'])
-        self.y = np.asarray(f['y_coordinates'])
-        self.z = np.asarray(f['z_coordinates']) 
-        self.angles = np.asarray(f['angles']) 
-        num_theta = len(self.angles)//2
-        self.theta_z = np.zeros(1 + num_theta)
-        self.theta_z[1:] = self.angles[num_theta+1:]
-        
-       # assert int(self.lxd) == int(self.x[-2])
-        
-        
-        dx = self.x[1] - self.x[0]
-        
-        #self.x /= self.lxd; self.y /= self.lxd; self.z /= self.lxd
-        fnx, fny, fnz = len(self.x), len(self.y), len(self.z)
-        print('grid ', fnx, fny, fnz)
-        
-        
-        G = re.search('G(\d+\.\d+)', self.data_file).group(1)
-        UC = re.search('UC(\d+\.\d+)', self.data_file).group(1)
-        data_frames = int(re.search('frames(\d+)', self.data_file).group(1))+1
-        self.physical_params = {'G':G, 'UC':UC}
-        print(self.physical_params)
-        
-        
-        self.alpha_pde = np.asarray(f['alpha'])
+            fnx, fny, fnz = len(self.x), len(self.y), len(self.z)
+            print('grid ', fnx, fny, fnz)
+            
+            alpha_pde = np.asarray(f['alpha']).reshape((fnx, fny, fnz),order='F')    
+         
+         
+            x_list.append(self.x)
+            alpha_list.append(alpha_pde)
+
+
+            if rank == 0:
+                self.angles = np.asarray(f['angles']) 
+                num_theta = len(self.angles)//2
+                self.theta_z = np.zeros(1 + num_theta)
+                self.theta_z[1:] = self.angles[num_theta+1:]
+                
+                dx = self.x[1] - self.x[0]
+                G = re.search('G(\d+\.\d+)', self.data_file).group(1)
+                UC = re.search('UC(\d+\.\d+)', self.data_file).group(1)
+              #  data_frames = int(re.search('frames(\d+)', self.data_file).group(1))+1
+                self.physical_params = {'G':G, 'UC':UC}
+                print(self.physical_params)
+
+ 
+        self.x = np.concatenate(x_list)
+        self.alpha_pde = np.concatenate(alpha_list, axis=0)
+        print('x, y, z', self.x.shape, self.y.shape, self.z.shape)
+        print('alpha_pde', self.alpha_pde.shape)
+ 
+    
         top_z = int(np.round(self.height/dx))
         
         if self.height == -1:
-            self.alpha_pde = self.alpha_pde.reshape((fnx, fny,fnz),order='F')[1:-1, 1:-1, 1:-1]
+            self.alpha_pde = self.alpha_pde[1:-1, 1:-1, 1:-1]
         
         else:
-            self.alpha_pde = self.alpha_pde.reshape((fnx, fny,fnz),order='F')[1:-1, 1:-1, 1:top_z]       
+            self.alpha_pde = self.alpha_pde[1:-1, 1:-1, 1:top_z]       
         
+
         
         print(len(np.unique(self.alpha_pde)), np.unique(self.alpha_pde))
       #  self.alpha_pde[self.alpha_pde == 0] = np.nan
         angle_pde = (self.alpha_pde<=num_theta)*self.alpha_pde + (self.alpha_pde>num_theta)*(self.alpha_pde%num_theta+1) 
         self.alpha_pde = self.theta_z[angle_pde]/pi*180
         
-       # print(self.alpha_pde)
-        if self.rank == 0:
-           origin = (self.x[1], self.y[1], self.z[1]) 
-        else:
-           origin = (self.x[0], self.y[1], self.z[1])
-        print(origin)
-        subsample = 2
-        self.alpha_pde = self.alpha_pde[::subsample,::subsample,::subsample]
-        dx = dx*subsample
+        origin = (self.x[1], self.y[1], self.z[1]) 
+
+    
+        self.alpha_pde = self.alpha_pde[::self.subsample,::self.subsample,::self.subsample]
+        dx = dx*self.subsample
         grid = tvtk.ImageData(spacing=(dx, dx, dx), origin=origin, 
                               dimensions=self.alpha_pde.shape)
         
@@ -98,7 +108,7 @@ class grain_visual:
         
         
         
-        self.dataname = rawdat_dir + '/seed'+str(self.seed) + '_rank' + str(self.rank) + '_time'+ str(self.time) +'.vtk'
+        self.dataname = rawdat_dir + '/seed'+str(self.seed) + '_time'+ str(self.time) +'.vtk'
                    #rawdat_dir + 'seed'+str(self.seed)+'_G'+str('%2.2f'%self.physical_params['G'])\
                    #+'_R'+str('%2.2f'%self.physical_params['R'])+'.vtk'
         write_data(grid, self.dataname)
@@ -116,16 +126,16 @@ if __name__ == '__main__':
     parser.add_argument("--mode", type=str, default='truth')
     
     parser.add_argument("--seed", type=int, default = 0)
-    parser.add_argument("--rank", type=int, default = 0)
+    parser.add_argument("--gpus", type=int, default = 0)
     parser.add_argument("--time", type=int, default = 0)
     parser.add_argument("--lxd", type=int, default = 40)
     parser.add_argument("--height", type=int, default = -1)
-    parser.add_argument("--subsample", type=int, default = 1)
+    parser.add_argument("--subsample", type=int, default = 2)
 
     args = parser.parse_args()
         
    # Gv = grain_visual(lxd = 20, seed = args.seed, height=20)   
-    Gv = grain_visual(rank = args.rank, lxd=args.lxd, seed=args.seed, height=args.height, time=args.time)  
+    Gv = grain_visual(gpus = args.gpus, lxd=args.lxd, seed=args.seed, height=args.height, time=args.time, subsample=args.subsample)  
     if args.mode == 'truth':
         
         Gv.load(rawdat_dir=args.rawdat_dir)  
